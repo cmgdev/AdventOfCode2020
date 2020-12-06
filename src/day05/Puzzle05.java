17535a1f9900a7026f0e1b0aa21707a930daa178
cmgdev <cmgdev@github.com>
2020-12-06 17:13:33 -0600

    day 5 part 2

--- src/day05/Puzzle05.java
+++ src/day05/Puzzle05.java
@@ -3,6 +3,7 @@ package day05;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import base.AbstractPuzzle;
 
@@ -17,6 +18,27 @@ public class Puzzle05 extends AbstractPuzzle {
 
 	@Override
 	public void solve1() {
+		Map<String, Integer> allSeatCodes = getAllSeatCodes();
+		List<String> seatCodes = getInput();
+		int highestSeatId = 0;
+
+		for (String seatCode : seatCodes) {
+			int seatId = getSeatId(allSeatCodes, seatCode);
+			highestSeatId = Math.max(seatId, highestSeatId);
+		}
+
+		System.out.println("Highest Seat Id: " + highestSeatId);
+		System.out.println(highestSeatId == getAnswer1());
+	}
+
+	private int getSeatId(Map<String, Integer> allSeatCodes, String seatCode) {
+		String rowCode = seatCode.substring(0, 7);
+		String colCode = seatCode.substring(7);
+		int seatId = (allSeatCodes.get(rowCode) * 8) + allSeatCodes.get(colCode);
+		return seatId;
+	}
+
+	private Map<String, Integer> getAllSeatCodes() {
 		Map<String, Integer> allSeatCodes = new HashMap<>();
 		for (int i = 0; i < 128; i++) {
 			allSeatCodes.put(getCode(i, 0, 127, "F", "B"), i);
@@ -24,18 +46,7 @@ public class Puzzle05 extends AbstractPuzzle {
 		for (int i = 0; i < 8; i++) {
 			allSeatCodes.put(getCode(i, 0, 7, "L", "R"), i);
 		}
-
-		List<String> seatCodes = getInput();
-		int highestSeatId = 0;
-		for (String seatCode : seatCodes) {
-			String rowCode = seatCode.substring(0, 7);
-			String colCode = seatCode.substring(7);
-			int seatId = (allSeatCodes.get(rowCode) * 8) + allSeatCodes.get(colCode);
-			highestSeatId = Math.max(seatId, highestSeatId);
-		}
-
-		System.out.println("Highest Seat Id: " + highestSeatId);
-		System.out.println(highestSeatId == getAnswer1());
+		return allSeatCodes;
 	}
 
 	public String getCode(int number, int lowIdx, int highIdx, String lowCode, String highCode) {
@@ -52,8 +63,24 @@ public class Puzzle05 extends AbstractPuzzle {
 
 	@Override
 	public void solve2() {
-		// TODO Auto-generated method stub
+		Map<String, Integer> allSeatCodes = getAllSeatCodes();
+		List<String> seatCodes = getInput();
 
+		List<Integer> seatIds = seatCodes.stream().map(code -> getSeatId(allSeatCodes, code)).sorted().collect(Collectors.toList());
+		int yourSeat = 0;
+		for (int i = 0; i < seatIds.size() - 1; i++) {
+			int seatBefore = seatIds.get(i);
+			int seatAfter = seatIds.get(i + 1);
+			if (seatAfter - seatBefore == 2) {
+				yourSeat = seatBefore + 1;
+				System.out.println("Before:" + seatBefore);
+				System.out.println("You:" + yourSeat );
+				System.out.println("After:" + seatAfter);
+				System.out.println("---");
+			}
+		}
+		System.out.println("Your seat: " + yourSeat);
+		System.out.println(yourSeat == getAnswer2());
 	}
 
 	public static void main(String[] args) {
