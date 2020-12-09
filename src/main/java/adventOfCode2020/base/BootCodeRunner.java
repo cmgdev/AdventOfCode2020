125024ad26ae43bf4593609115fffdc37481d62a
cmgdev <cmgdev@github.com>
2020-12-08 22:16:39 -0600

    refactored because surely there will be more puzzles building on this "boot code"

--- src/main/java/adventOfCode2020/base/BootCodeRunner.java
+++ src/main/java/adventOfCode2020/base/BootCodeRunner.java
@@ -1,9 +1,159 @@
 package adventOfCode2020.base;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class BootCodeRunner {
 
-    public BootCodeRunner() {
-        // TODO Auto-generated constructor stub
+    Instructions instructions;
+    CurrentRuntime currentRuntime;
+    StatusCode currentStatus;
+    boolean stopOnInfiniteLoop = false;
+
+    public BootCodeRunner(List<String> instructions) {
+        this.currentStatus = StatusCode.RUNNING;
+        this.instructions = new Instructions(instructions);
+        this.currentRuntime = new CurrentRuntime(instructions.size());
     }
 
+    public void setStopOnInfiniteLoop(boolean stopOnInfiniteLoop) {
+        this.stopOnInfiniteLoop = stopOnInfiniteLoop;
+    }
+
+    public void run() {
+        instructions.run(currentRuntime, stopOnInfiniteLoop);
+    }
+
+    public int getAccumulatorValue() {
+        return this.currentRuntime.getAccumlatorValue();
+    }
+
+    enum Operation {
+        ACC, JMP, NOP;
+    }
+
+    enum StatusCode {
+        EXIT_SUCCESS, EXIT_INFINITE_LOOP, EXIT_INDEX_OUT_OF_BOUNDS, RUNNING;
+    }
+
+    class Instructions {
+        List<String> originalInstructions = new ArrayList<>();
+        List<Instruction> instructions = new ArrayList<>();
+        Set<Integer> modifiedLines = new HashSet<>();
+
+        public Instructions(List<String> instructions) {
+            this.originalInstructions = instructions;
+            resetInstructions();
+        }
+
+        public void run(CurrentRuntime currentRuntime, boolean stopOnInfiniteLoop) {
+            while (!currentRuntime.finishedInstructions()) {
+                if (currentRuntime.currentLineAlreadyExecuted()) {
+                    if (stopOnInfiniteLoop) {
+                        return;
+                    }
+                    flipInstructions();
+                    currentRuntime.reset();
+                }
+                instructions.get(currentRuntime.getCurrentLine()).run(currentRuntime);
+            }
+        }
+
+        protected void resetInstructions() {
+            this.instructions.clear();
+            originalInstructions.forEach(i -> this.instructions.add(new Instruction(i)));
+        }
+
+        public void flipInstructions() {
+            resetInstructions();
+
+            for (int i = 0; i < instructions.size(); i++) {
+                Instruction instruction = instructions.get(i);
+                if (Operation.NOP.equals(instruction.operation) && !modifiedLines.contains(i)) {
+                    instruction.operation = Operation.JMP;
+                    modifiedLines.add(i);
+                    return;
+                } else if (Operation.JMP.equals(instruction.operation) && !modifiedLines.contains(i)) {
+                    instruction.operation = Operation.NOP;
+                    modifiedLines.add(i);
+                    return;
+                }
+            }
+        }
+    }
+
+    class Instruction {
+        Operation operation;
+        int argument;
+
+        public Instruction(String instruction) {
+            this.operation = getInstructionOperation(instruction);
+            this.argument = getInstructionArg(instruction);
+        }
+
+        protected Operation getInstructionOperation(String instruction) {
+            return Operation.valueOf(instruction.split(" ")[0].toUpperCase());
+        }
+
+        protected int getInstructionArg(String instruction) {
+            return Integer.parseInt(instruction.split(" ")[1]);
+        }
+
+        public void run(CurrentRuntime runtime) {
+            if (Operation.NOP.equals(operation)) {
+                currentRuntime.incrementCurrentLine(1);
+            } else if (Operation.ACC.equals(operation)) {
+                currentRuntime.incrementAccumlator(argument);
+                currentRuntime.incrementCurrentLine(1);
+            } else if (Operation.JMP.equals(operation)) {
+                currentRuntime.incrementCurrentLine(argument);
+            }
+        }
+    }
+
+    class CurrentRuntime {
+
+        int currentLine = 0;
+        int totalLines = 0;
+        int accumlatorValue = 0;
+        Set<Integer> executedLines = new HashSet<>();
+
+        public CurrentRuntime(int totalLines) {
+            this.totalLines = totalLines;
+        }
+
+        public void reset() {
+            this.currentLine = 0;
+            this.accumlatorValue = 0;
+            this.executedLines.clear();
+        }
+
+        public void incrementCurrentLine(int amount) {
+            this.executedLines.add(this.currentLine);
+            this.currentLine += amount;
+        }
+
+        public void incrementAccumlator(int amount) {
+            this.accumlatorValue += amount;
+        }
+
+        public int getCurrentLine() {
+            return currentLine;
+        }
+
+        public int getAccumlatorValue() {
+            return this.accumlatorValue;
+        }
+
+        public boolean currentLineAlreadyExecuted() {
+            return this.executedLines.contains(this.currentLine);
+        }
+
+        public boolean finishedInstructions() {
+            return this.currentLine == totalLines;
+        }
+
+    }
 }
--- src/main/java/adventOfCode2020/day08/Puzzle08.java
+++ src/main/java/adventOfCode2020/day08/Puzzle08.java
@@ -1,15 +1,11 @@
 package adventOfCode2020.day08;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import adventOfCode2020.base.AbstractPuzzle;
+import adventOfCode2020.base.BootCodeRunner;
 
 public class Puzzle08 extends AbstractPuzzle {
-
-    private static final String JMP = "jmp";
-    private static final String ACC = "acc";
-    private static final String NOP = "nop";
 
     public Puzzle08(boolean isTest) {
         super(isTest);
@@ -18,27 +14,12 @@ public class Puzzle08 extends AbstractPuzzle {
     @Override
     public Object solve1() {
         List<String> instructions = getInput();
-        int currentLine = 0;
-        int accumlatorValue = 0;
-        List<Integer> executedLines = new ArrayList<>();
-
-        while (!executedLines.contains(currentLine)) {
-            String instruction = instructions.get(currentLine);
-            String operation = getInstructionOperation(instruction);
-            int arg = getInstructionArg(instruction);
-            int nextLine = currentLine;
-
-            if (NOP.equals(operation)) {
-                nextLine++;
-            } else if (ACC.equals(operation)) {
-                accumlatorValue += arg;
-                nextLine++;
-            } else if (JMP.equals(operation)) {
-                nextLine += arg;
-            }
-            executedLines.add(currentLine);
-            currentLine = nextLine;
-        }
+        
+        BootCodeRunner bootCodeRunner = new BootCodeRunner(instructions);
+        bootCodeRunner.setStopOnInfiniteLoop(true);
+        bootCodeRunner.run();
+        
+        int accumlatorValue = bootCodeRunner.getAccumulatorValue();
 
         System.out.println(accumlatorValue);
         return accumlatorValue;
@@ -47,89 +28,14 @@ public class Puzzle08 extends AbstractPuzzle {
     @Override
     public Object solve2() {
         List<String> instructions = readFile();
-        int instructionCount = instructions.size();
-        int currentLine = 0;
-        int accumlatorValue = 0;
-        List<Integer> executedLines = new ArrayList<>();
-        List<Integer> modifiedLines = new ArrayList<>();
 
-        int statusExitSuccess = 0;
-        int statusExitInfiniteLoop = 1;
-        int statusExitIdxOutOfBounds = 2;
-        int statusRunning = 4;
-        int currentStatus = statusRunning;
-
-        while (currentStatus == statusRunning) {
-
-            // hit infinite loop, reset counters and flip an instruction
-            if (executedLines.contains(currentLine)) {
-                System.out.println("Hit infinite loop!");
-//                currentStatus = statusExitInfiniteLoop;
-
-                currentLine = 0;
-                accumlatorValue = 0;
-                instructions = readFile();
-                executedLines.clear();
-
-                boolean flippedAnOp = false;
-
-                for (int i = 0; i < instructionCount; i++) {
-                    String instruction = instructions.get(i);
-                    if (NOP.equals(getInstructionOperation(instruction)) && !modifiedLines.contains(i)) {
-                        instruction = instruction.replace(NOP, JMP);
-                        flippedAnOp = true;
-                    } else if (JMP.equals(getInstructionOperation(instruction)) && !modifiedLines.contains(i)) {
-                        instruction = instruction.replace(JMP, NOP);
-                        flippedAnOp = true;
-                    }
-                    if (flippedAnOp) {
-                        modifiedLines.add(i);
-                        instructions.remove(i);
-                        instructions.add(i, instruction);
-                        break;
-                    }
-                }
-
-            } 
-            
-            if (currentLine == instructionCount) {
-                currentStatus = statusExitSuccess;
-                break;
-            } else if (currentLine > instructionCount) {
-                System.out.println("Oh no! Ran over the end with line " + currentLine);
-                currentStatus = statusExitIdxOutOfBounds;
-                break;
-            }
-
-            String instruction = instructions.get(currentLine);
-            String operation = getInstructionOperation(instruction);
-            int arg = getInstructionArg(instruction);
-            int nextLine = currentLine;
-
-            if (NOP.equals(operation)) {
-                nextLine++;
-            } else if (ACC.equals(operation)) {
-                accumlatorValue += arg;
-                nextLine++;
-            } else if (JMP.equals(operation)) {
-                nextLine += arg;
-            }
-            executedLines.add(currentLine);
-            currentLine = nextLine;
-
-        }
-
+        BootCodeRunner bootCodeRunner = new BootCodeRunner(instructions);
+        bootCodeRunner.run();
+        
+        int accumlatorValue = bootCodeRunner.getAccumulatorValue();
         System.out.println(accumlatorValue);
         return accumlatorValue;
 
-    }
-
-    protected String getInstructionOperation(String instruction) {
-        return instruction.split(" ")[0];
-    }
-
-    protected int getInstructionArg(String instruction) {
-        return Integer.parseInt(instruction.split(" ")[1]);
     }
 
 }
