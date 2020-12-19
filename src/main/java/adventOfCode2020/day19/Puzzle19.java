8b3e382cf8c8cca6835522f6197d8ad3df497c4f
cmgdev <cmgdev@github.com>
2020-12-19 01:50:03 -0600

    day 19 part 2

--- src/main/java/adventOfCode2020/day19/Puzzle19.java
+++ src/main/java/adventOfCode2020/day19/Puzzle19.java
@@ -1,6 +1,5 @@
 package adventOfCode2020.day19;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -65,10 +64,67 @@ public class Puzzle19 extends AbstractPuzzle {
         return sb.toString();
     }
 
+    protected String getRegex2(int ruleNum, Map<Integer, String> rules) {
+        if (ruleNum == 8) {
+            return "(" + getRegex2(42, rules) + "+)";
+        }
+        if (ruleNum == 11) {
+            String fourtyTwo = getRegex2(42, rules);
+            String thirtyOne = getRegex2(31, rules);
+            StringBuilder sb11 = new StringBuilder("(");
+            for (int i = 1; i <= 5; i++) {
+                sb11.append("(");
+                for (int j = 0; j < i; j++) {
+                    sb11.append(fourtyTwo);
+                }
+                for (int j = 0; j < i; j++) {
+                    sb11.append(thirtyOne);
+                }
+                sb11.append(")");
+                if (i < 5) {
+                    sb11.append("|");
+                }
+            }
+            sb11.append(")");
+            return sb11.toString();
+        }
+
+        StringBuilder sb = new StringBuilder("(");
+        String rule = rules.get(ruleNum);
+        String[] tokens = rule.split(" ");
+        for (String token : tokens) {
+            if (token.matches("[a-z]")) {
+                return token;
+            } else if (token.equals("|")) {
+                sb.append("|");
+            } else {
+                sb.append(getRegex2(Integer.parseInt(token), rules));
+            }
+        }
+
+        sb.append(")");
+        return sb.toString();
+    }
+
     @Override
     public Object solve2() {
         List<String> inputs = getInput();
-        return null;
+
+        Map<Integer, String> rules = getRules(inputs);
+        List<String> messages = inputs.stream().filter(i -> !i.contains(":")).collect(Collectors.toList());
+
+        String regex = getRegex2(0, rules);
+
+        System.out.println(regex);
+
+        long matches = 0;
+        for (String message : messages) {
+            if (message.matches(regex)) {
+                matches++;
+            }
+        }
+
+        return matches;
     }
 
 }
