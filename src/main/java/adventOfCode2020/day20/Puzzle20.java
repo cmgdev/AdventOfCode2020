7d4b1beb8ea223ae520da8b712e19baa35d8e129
cmgdev <cmgdev@github.com>
2020-12-20 22:34:29 -0600

    day 20 part 2 in progress

--- src/main/java/adventOfCode2020/day20/Puzzle20.java
+++ src/main/java/adventOfCode2020/day20/Puzzle20.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -20,6 +21,35 @@ public class Puzzle20 extends AbstractPuzzle {
 
     @Override
     public Object solve1() {
+        List<Tile> tiles = getTiles();
+
+        findNeighbors(tiles);
+
+        List<Integer> cornerIds = tiles.stream().filter(t -> t.neighbors.size() == 2).map(t -> t.id).collect(Collectors.toList());
+        long result = 1;
+        if (cornerIds.size() == 4) {
+            for (int id : cornerIds) {
+                result *= id;
+            }
+        }
+
+        return result;
+    }
+
+    protected void findNeighbors(List<Tile> tiles) {
+        for (int i = 0; i < tiles.size() - 1; i++) {
+            Tile ti = tiles.get(i);
+            for (int j = i + 1; j < tiles.size(); j++) {
+                Tile tj = tiles.get(j);
+                if (!Collections.disjoint(ti.getEdges(), tj.getEdges()) || !Collections.disjoint(ti.getFlippedEdges(), tj.getEdges())) {
+                    ti.neighbors.add(tj.id);
+                    tj.neighbors.add(ti.id);
+                }
+            }
+        }
+    }
+
+    protected List<Tile> getTiles() {
         List<String> input = readFile("//", false);
 
         String header = "Tile [0-9]+:";
@@ -39,67 +69,113 @@ public class Puzzle20 extends AbstractPuzzle {
             }
             tiles.add(new Tile(id, rows));
         }
-
-        for (int i = 0; i < tiles.size() - 1; i++) {
-            Tile ti = tiles.get(i);
-            for (int j = i + 1; j < tiles.size(); j++) {
-                Tile tj = tiles.get(j);
-                if (!Collections.disjoint(ti.edges, tj.edges) || !Collections.disjoint(ti.getFlippedEdges(), tj.edges)) {
-                    ti.neighbors.add(tj.id);
-                    tj.neighbors.add(ti.id);
-                }
-            }
-        }
-
-        List<Integer> cornerIds = tiles.stream().filter(t -> t.neighbors.size() == 2).map(t -> t.id).collect(Collectors.toList());
-        long result = 1;
-        if (cornerIds.size() == 4) {
-            for (int id : cornerIds) {
-                result *= id;
-            }
-        }
-
-        System.out.println(tiles);
-        return result;
+        return tiles;
     }
 
     @Override
     public Object solve2() {
-        // TODO Auto-generated method stub
-        return null;
+        List<Tile> tiles = getTiles();
+
+        findNeighbors(tiles);
+
+        List<Integer> cornerIds = tiles.stream().filter(t -> t.neighbors.size() == 2).map(t -> t.id).collect(Collectors.toList());
+
+        int numTiles = tiles.size();
+        int[][] fullImage = new int[numTiles / 2][numTiles / 2];
+        int row = 0;
+        int col = 0;
+
+        int processedTiles = 1;
+        Tile current = getById(tiles, cornerIds.get(0));
+        fullImage[row][col] = current.id;
+
+        List<Tile> neighbors = current.neighbors.stream().map(n -> getById(tiles, n)).collect(Collectors.toList());
+        Tile neighbor = neighbors.get(0);
+        for (int i = 0; i < 4; i++) {
+            String currentEdge = current.getEdges().get(i);
+            if (neighbor.getEdges().contains(currentEdge)) {
+                current.quarterTurns = i;
+                neighbor.quarterTurns = (neighbor.getEdges().indexOf(currentEdge) + 1) % 4;
+                fullImage[row][col + 1] = neighbor.id;
+                processedTiles++;
+                break;
+            }
+        }
+
+        printTilesWithEdges(fullImage, tiles);
+
+        int seaRoughness = 0;
+        return seaRoughness;
+    }
+
+    protected void printTilesWithEdges(int[][] tileIds, List<Tile> tiles) {
+        int tileHeight = tiles.get(0).image.length;
+        for (int r = 0; r < tileIds.length; r++) {
+            if (tileIds[r] != null) {
+                int[] row = tileIds[r];
+                List<Tile> thisRow = IntStream.range(0, row.length).filter(col -> row[col] > 0).mapToObj(col -> getById(tiles, row[col])).collect(Collectors.toList());
+                for (Tile t : thisRow) {
+                    System.out.print("Tile " + t.id + "\t");
+                }
+                System.out.println();
+                for (int i = 0; i < tileHeight; i++) {
+                    for (Tile t : thisRow) {
+                        System.out.print(new String(t.image[i]) + "\t");
+                    }
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    Tile getById(List<Tile> tiles, int id) {
+        return tiles.stream().filter(t -> t.id == id).findFirst().get();
     }
 
     class Tile {
         int id;
-        List<String> edges = new ArrayList<>();
         Set<Integer> neighbors = new HashSet<>();
+        char[][] image;
+        boolean isFlipped = false;
+        int quarterTurns = 0;
 
         public Tile(int id, List<String> rows) {
             this.id = id;
 
-            String topEdge = rows.get(0);
-            String bottomEdge = rows.get(rows.size() - 1);
+            image = new char[rows.size()][rows.get(0).length()];
+
+            for (int i = 0; i < rows.size(); i++) {
+                image[i] = rows.get(i).toCharArray();
+            }
+        }
+
+        List<String> getEdges() {
+            int rows = image.length;
+            int cols = image[0].length;
+
+            String topEdge = new String(image[0]);
+            String bottomEdge = new String(image[rows - 1]);
             StringBuilder rightEdge = new StringBuilder();
             StringBuilder leftEdge = new StringBuilder();
-            int colSize = topEdge.length();
 
-            for (String row : rows) {
-                leftEdge.append(row.charAt(0));
-                rightEdge.append(row.charAt(colSize - 1));
+            for (char[] row : image) {
+                leftEdge.append(row[0]);
+                rightEdge.append(row[cols - 1]);
             }
-            edges.addAll(Arrays.asList(topEdge, rightEdge.toString(), bottomEdge, leftEdge.toString()));
+
+            return Arrays.asList(topEdge, rightEdge.toString(), bottomEdge, leftEdge.toString());
         }
 
         List<String> getFlippedEdges() {
-            return edges.stream().map(e -> StringUtils.reverse(e)).collect(Collectors.toList());
+            return getEdges().stream().map(e -> StringUtils.reverse(e)).collect(Collectors.toList());
         }
 
         @Override
         public int hashCode() {
             final int prime = 31;
             int result = 1;
-            result = prime * result + ((edges == null) ? 0 : edges.hashCode());
             result = prime * result + id;
+            result = prime * result + Arrays.deepHashCode(image);
             result = prime * result + ((neighbors == null) ? 0 : neighbors.hashCode());
             return result;
         }
@@ -113,12 +189,9 @@ public class Puzzle20 extends AbstractPuzzle {
             if (getClass() != obj.getClass())
                 return false;
             Tile other = (Tile) obj;
-            if (edges == null) {
-                if (other.edges != null)
-                    return false;
-            } else if (!edges.equals(other.edges))
-                return false;
             if (id != other.id)
+                return false;
+            if (!Arrays.deepEquals(image, other.image))
                 return false;
             if (neighbors == null) {
                 if (other.neighbors != null)
@@ -130,8 +203,9 @@ public class Puzzle20 extends AbstractPuzzle {
 
         @Override
         public String toString() {
-            return "Tile [id=" + id + ", edges=" + edges + ", neighbors=" + neighbors + "]";
+            return "Tile [id=" + id + ", neighbors=" + neighbors + ", image=" + Arrays.toString(image) + "]";
         }
+
     }
 
 }
