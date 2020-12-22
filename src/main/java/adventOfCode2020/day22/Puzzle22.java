bbaa797fe2cfde1bf1021deda37242c4e685fa21
cmgdev <cmgdev@github.com>
2020-12-22 01:51:05 -0600

    day 22 part 2

--- src/main/java/adventOfCode2020/day22/Puzzle22.java
+++ src/main/java/adventOfCode2020/day22/Puzzle22.java
@@ -1,7 +1,11 @@
 package adventOfCode2020.day22;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import adventOfCode2020.base.AbstractPuzzle;
 
@@ -27,11 +31,8 @@ public class Puzzle22 extends AbstractPuzzle {
             }
         }
 
-        int totalCards = deck1.size() + deck2.size();
-        boolean keepPlaying = true;
         int round = 1;
-
-        while (keepPlaying) {
+        while (!deck1.isEmpty() && !deck2.isEmpty()) {
             System.out.println("-- Round " + round + " --");
             System.out.println("Player 1's deck: " + deck1);
             System.out.println("Player 2's deck: " + deck2);
@@ -52,16 +53,17 @@ public class Puzzle22 extends AbstractPuzzle {
             }
 
             round++;
-            if (deck1.size() == totalCards || deck2.size() == totalCards) {
-                keepPlaying = false;
-            }
         }
 
-        LinkedList<Integer> winningDeck = deck1.size() == totalCards ? deck1 : deck2;
+        return getScoreOfWinningDeck(deck1, deck2);
+    }
+
+    protected long getScoreOfWinningDeck(LinkedList<Integer> deck1, LinkedList<Integer> deck2) {
+        LinkedList<Integer> winningDeck = deck1.size() == 0 ? deck2 : deck1;
 
         int points = 1;
         long total = 0;
-        for( int i = winningDeck.size() - 1; i >= 0; i--) {
+        for (int i = winningDeck.size() - 1; i >= 0; i--) {
             total += (points * winningDeck.get(i));
             points++;
         }
@@ -71,8 +73,68 @@ public class Puzzle22 extends AbstractPuzzle {
 
     @Override
     public Object solve2() {
-        // TODO Auto-generated method stub
-        return null;
+        List<String> input = getInput();
+        LinkedList<Integer> deck1 = new LinkedList<>();
+        LinkedList<Integer> deck2 = new LinkedList<>();
+        LinkedList<Integer> currentDeck = deck1;
+
+        for (int i = 1; i < input.size(); i++) {
+            String card = input.get(i);
+            if (card.matches("^[0-9]+$")) {
+                currentDeck.add(Integer.parseInt(card));
+            } else {
+                currentDeck = deck2;
+            }
+        }
+
+        int winner = playCrabCombat(deck1, deck2);
+        System.out.println("Player " + winner + " won!");
+
+        return getScoreOfWinningDeck(deck1, deck2);
+    }
+
+    public int playCrabCombat(LinkedList<Integer> deck1, LinkedList<Integer> deck2) {
+        Set<List<List<Integer>>> allHands = new HashSet<>();
+
+        while (!deck1.isEmpty() && !deck2.isEmpty()) {
+
+            List<List<Integer>> thisHand = Arrays.asList(new ArrayList<>(deck1), new ArrayList<>(deck2));
+            if (allHands.contains(thisHand)) {
+                return 1;
+            }
+            allHands.add(thisHand);
+
+            int card1 = deck1.remove();
+            int card2 = deck2.remove();
+
+            boolean player1Wins = false;
+            if (card1 > deck1.size() || card2 > deck2.size() ) {
+                player1Wins = card1 > card2;
+            }
+
+            else {
+                LinkedList<Integer> newDeck1 = new LinkedList<>();
+                LinkedList<Integer> newDeck2 = new LinkedList<>();
+                for (int i = 0; i < card1; i++) {
+                    newDeck1.add(deck1.get(i));
+                }
+                for (int i = 0; i < card2; i++) {
+                    newDeck2.add(deck2.get(i));
+                }
+                player1Wins = playCrabCombat(newDeck1, newDeck2) == 1;
+            }
+
+            if (player1Wins) {
+                deck1.add(card1);
+                deck1.add(card2);
+            } else {
+                deck2.add(card2);
+                deck2.add(card1);
+            }
+
+        }
+
+        return deck1.isEmpty() ? 2 : 1;
     }
 
 }
